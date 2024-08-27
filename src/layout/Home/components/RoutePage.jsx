4775cb480b5885d535f597f063db45c147640359
MurotjonAbidjanov <murotzonabidzanov@gmail.com>
2024-08-28 04:08:02 +0600

    add routpage

---
+++ src/layout/Home/components/RoutePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { routes } from "../../../config/routes";
+import UsersPage from "../../../modules/Users/UsersPage";
+
+export default function RoutePage() {
+  return (
+    <div className="routes">
+      <Routes>
+        <Route index element={<UsersPage />} />
+        {routes.map((route) => (
+          <Route key={route.id} path={route.path} element={route.element} />
+        ))}
+      </Routes>
+    </div>
+  );
+}
