4f8f917f49c7433f5362da791173f16f17d48030
asadchikit <mirbabaevsolihiddin@gmail.com>
2024-08-28 00:43:17 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import HomePage from './layout'
 
 export default function App() {
   return (
     <div>
-      <HomePage />
     </div>
   )
 }
--- src/modules/Orders/pages/OrdersLook/OrdersLook.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function OrdersLook() {
-  return (
-    <div>OrdersLook</div>
-  )
-}
